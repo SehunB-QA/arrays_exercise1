e03e184e0c4c8890d43ec81f80acadb8394e813f
SehunB-QA <sbabatunde@qa.com>
2021-01-11 00:37:53 +0000

    Added QA Java  Array Exercise 1

---
+++ src/arrays_exercise1/ArraysProgram.java
@@ -0,0 +1,17 @@
+package arrays_exercise1;
+
+public class ArraysProgram {
+
+	
+	public void ArrayTenValues() 
+	{
+		int[] arrayTens=  {1,2,3,4,5,6,7,8,9,10}; 
+		
+		 for(int i = 0; i < arrayTens.length; i++) 
+		 {
+	            System.out.println(arrayTens[i]);
+		
+	}
+		 
+	}
+}
---
+++ src/arrays_exercise1/Main.java
@@ -0,0 +1,12 @@
+package arrays_exercise1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArraysProgram ap = new ArraysProgram();
+		ap.ArrayTenValues();
+
+	}
+
+}
